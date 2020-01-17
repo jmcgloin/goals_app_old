cfe5ea1e98e87d929adb9a56a6bef1a5030b27cb
Jason McGloin <jason.mcgloin@gmail.com>
2020-01-17 02:25:04 -0500

    creates register form, register submit, fetch to post new user

--- goals-app-client/assets/javascript/index.js
+++ goals-app-client/assets/javascript/index.js
@@ -1,21 +1,60 @@
+/*
+TODO:
+
+x1. on load, check if there is an active session (only work with no active session for now)
+2. after verifying no session, show the register/login screen (register done, work with login)
+create login form
+3. show login form
+4. upon completion of form, post to session#create to auth user (check that exists for now)
+
+*/
+
 document.addEventListener('DOMContentLoaded', () => {
+	document.getElementById('register-form').addEventListener('submit', registerUser)
 	isLoggedIn()
-	// updateUser({username: 'Jason', id: 1})
 })
 
-const getUser = () => {
-	fetch('http://localhost:3000/users/2')
-		.then(r => r.json())
-		.then(rj => displayUserInfo(rj))
+class User {
+	constructor(username, email) {
+		this.username = username
+		this.email = email
+		this.password = null
+	}
+
+	showInfo() {
+
+	}
+
 }
 
-const updateUser = (newInfo) => {
-	fetch('http://localhost:3000/users/1', {
-		method: 'PATCH',
+const isLoggedIn = () => {
+	fetch('http://localhost:3000/session')
+		.then(r => r.json())
+		.then(rj => {
+			if(rj.logged == 'false') {
+				showLogInRegister()
+			} else {
+				//stuff to do if logged in
+			}
+		})
+}
+
+const showLogInRegister = () => {
+	const logInRegisterDiv = document.getElementById('login-register')
+	logInRegisterDiv.classList.remove('hidden')
+}
+
+const registerUser = (e) => {
+	e.preventDefault()
+	const user = new User(e.target[name='username'].value, e.target[name='email'].value)
+	user.password = e.target[name='password'].value
+	clearFields(e.target.getElementsByTagName('input'))
+	fetch('http://localhost:3000/users', {
+		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json'
 		},
-		body: JSON.stringify(newInfo)
+		body: JSON.stringify(user)
 	})
 		.then(r => r.json())
 		.then(rj => console.log(rj))
@@ -33,36 +72,24 @@ const displayUserInfo = (info) => {
 	// console.log(info)
 }
 
-const showLogIn = () => {
-	const logInDiv = document.getElementById('login')
-	logInDiv.classList.remove('hidden')
-}
 
-const isLoggedIn = () => {
-	fetch('http://localhost:3000/session')
-		.then(r => r.json())
-		.then(rj => {
-			if(rj.logged == 'false') {
-				showLogIn()
-			} else {
-				console.log(rj)
-				createUser(rj)
-			}
-		})
-}
 
 const createUser = (info) => {
 	const user = new User(info.username, info.goals)
 }
 
-class User {
-	constructor(username, goals) {
-		this.username = username
-		this.goals = goals
-	}
+const updateUser = (newInfo) => {
+	fetch('http://localhost:3000/users/1', {
+		method: 'PATCH',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(newInfo)
+	})
+		.then(r => r.json())
+		.then(rj => console.log(rj))
+}
 
-	showInfo() {
-
-	}
-
+const clearFields = (collection) => {
+	Array.from(collection).forEach(input => input.value = input.type != 'submit' ? '' : 'Register')
 }
